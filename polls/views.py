77c0fcf12964ae96fc3e71dbba39fd99fcbcadf1
Austin Brown <abrown1357@live.com>
2017-04-12 13:42:31 +0000

    added veiw and template to throw a 404 error when we are looking beyond the number of questions we actually have and to add the choices to our questions

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Question
 
@@ -12,10 +12,12 @@ def index(request):
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)    
     
+def detail(request, question_id):#404 error
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})    
     
-    
-def detail(request, question_id):#primary key number
-    return HttpResponse("You're looking at question %s." % question_id)#will print question ID
+#def detail(request, question_id):#primary key number
+   # return HttpResponse("You're looking at question %s." % question_id)#will print question ID
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."

4948f3ea3b3af3e3e755db7e50d65a2ab3875244
Austin Brown <abrown1357@live.com>
2017-04-12 05:19:05 +0000

    added more to admin interface for models

--- polls/views.py
+++ polls/views.py
@@ -5,3 +5,13 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hey")
+    
+def detail(request, question_id):#primary key number
+    return HttpResponse("You're looking at question %s." % question_id)#will print question ID
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
